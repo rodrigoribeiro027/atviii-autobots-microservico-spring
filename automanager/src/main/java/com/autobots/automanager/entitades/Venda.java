0edc561628f0cb14bc38f573ff33c03bc17482c9
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-11-30 14:31:08 -0300

    Listaiii

---
+++ automanager/src/main/java/com/autobots/automanager/entitades/Venda.java
@@ -0,0 +1,47 @@
+package com.autobots.automanager.entitades;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(exclude = { "cliente", "funcionario", "veiculo" })
+@Entity
+public class Venda {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private Date cadastro;
+	@Column(nullable = false, unique = true)
+	private String identificacao;
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
+	@JsonIgnoreProperties(value = {"credencias", "mercadorias","vendas","veiculos"})
+	private Usuario cliente;
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
+	private Usuario funcionario;
+	@JsonIgnoreProperties(value = {"credencias", "mercadorias","vendas","veiculos"})
+	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
+	private Set<Mercadoria> mercadorias = new HashSet<>();
+	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
+	private Set<Servico> servicos = new HashSet<>();
+	@OneToOne(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
+	@JsonIgnoreProperties(value = {"vendas","proprietario"})
+	private Veiculo veiculo;
+}
